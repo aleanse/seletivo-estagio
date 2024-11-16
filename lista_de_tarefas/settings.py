ee281d8feabb762e89f8a7abb2928c56ab996ba3
aleanse dos santos lima rego <aleanse_54@hotmail.com>
2024-11-15 22:52:18 -0300

    preparando-deploy

--- lista_de_tarefas/settings.py
+++ lista_de_tarefas/settings.py
@@ -140,6 +140,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
+WHITENOISE_USE_FINDERS = True
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
