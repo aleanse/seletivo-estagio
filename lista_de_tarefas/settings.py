7f40be2fcfc439550e6396464ff49217be6d1181
aleanse dos santos lima rego <aleanse_54@hotmail.com>
2024-11-15 23:15:07 -0300

    preparando-deploy

--- lista_de_tarefas/settings.py
+++ lista_de_tarefas/settings.py
@@ -133,10 +133,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'tasks', 'static'),  # Adiciona o diretório estático do app task
+]
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
