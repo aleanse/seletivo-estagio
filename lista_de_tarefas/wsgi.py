1b52576731a94d128185d0f70d2a3a5b8ab80218
aleanse dos santos lima rego <aleanse_54@hotmail.com>
2024-11-15 23:10:36 -0300

    preparando-deploy

--- lista_de_tarefas/wsgi.py
+++ lista_de_tarefas/wsgi.py
@@ -10,9 +10,14 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+
+from lista_de_tarefas.settings import BASE_DIR
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lista_de_tarefas.settings')
 
 application = get_wsgi_application()
+
+application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
 
 app = application
